e48dc0b8f2ea5625eaae209d6f0620940e99ff90
pgrinaway <pgrinaway@gmail.com>
2017-10-16 16:22:20 -0400

    Implement equilibrium task callback
    This is the implementation of the first task handling a request for an
    equilibrium job. Although the code seems more complicated than celery,
    it enables the sort of very complex workflows that we require in this
    appliucation. To summarize the operation (this will go in the docs as
    well):
    
    1. Receive the incoming body, which is bytes
    2. unpack the components of that that will be required to run an
    equilibrium task
    3. Call the equilibrium task
    4. Publish different subsets of the results to different topics
    
    In particular, this enables a few desired things:
    
    1. We can route different parts of the results to different tasks,
    preventing the need to match returns and signatures
    2. We can actually have cycles in the compute graph. This function will
    call itself by sending a message to its own topic
    3. It enables listeners to capture whatever information they want on
    whatever topic they want without disturbing the calculation.
    
    It should be noted that this expects all the information necessary to
    run both an equilibrium and a nonequilibrium task. While we may be able
    to generate the necessary nonequilibrium stuff in this task, it seems a)
    messy and b) would still require additional flags.

---
+++ perses/distributed/fep_gpu_worker.py
@@ -0,0 +1,140 @@
+import perses.distributed.feptasks as feptasks
+import pika
+from pika import spec, channel
+from typing import Union
+import pickle
+from io import BytesIO
+
+
+def minimize_callback(ch: channel, method: spec.Basic.Deliver, properties: spec.BasicProperties, body: bytes):
+    """
+    The callback function that handles tasks placed in the minimize queue. The arguments to this function have a standard
+    form based on the AMQP client library pika. The data to be processed is in the body. This method acknowledges the completion
+    of its task.
+    """
+    pass
+
+
+
+def equilibrium_callback(ch: channel, method: spec.Basic.Deliver, properties: spec.BasicProperties, body: bytes):
+    """
+    The method that is called to process a request to run some equilibrium dynamics. The body needs to contain:
+    sampler_state, thermodynamic_state, mc_move, topology, n_iterations, n_cycles (this is the number of times to run the
+    equilibrium-nonequilbrium pairs), and optionally may contain atom_indices_to_save, which indicates that not all atoms
+    should be saved in the trajectory, as well as run_nonequilibrium. When called, this method will also follow up by calling
+    the nonequilibrium switching task, if requested. It will also call itself again if n_cycles > 0, and decrement n_cycles.
+
+    Parameters
+    ----------
+    ch : pika.channel
+    method : spec.Basic.Deliver
+    properties : spec.BasicProperties
+    body : bytes
+    """
+    #declare the exchanges through which we will send our results:
+    ch.exchange_declare(exchange='equilibrium', exchange_type="topic")
+    ch.exchange_declare(exchange='nonequilibrium', exchange_type='topic')
+    ch.exchange_declare(exchange='utilities', exchange_type='topic')
+
+    #get the routing key, since we are listening to all requests for equilibrium tasks:
+    routing_key = method.routing_key
+
+    #the routing key that we are listening to is *.*.equilibrium.resume
+    #the first wildcard is a calculation name, the second is the lambda state (0 or 1)
+    #this is used to direct tasks to the appropriate write and log queues
+    routing_key_parts = routing_key.split(".")
+    calculation_name = routing_key_parts[0]
+    lambda_value = routing_key_parts[1]
+
+    equilibrium_prefix_key = ".".join([calculation_name, lambda_value, "equilibrium"])
+    nonequilibrium_prefix_key = ".".join([calculation_name, lambda_value, "nonequilibrium"])
+
+    #first, we need to unpack the contents of the body:
+    input_data_bytesio = BytesIO(initial_bytes=body)
+    input_arguments = pickle.load(input_data_bytesio)
+
+    try:
+        thermodynamic_state = input_arguments['thermodynamic_state']
+        sampler_state = input_arguments['sampler_state']
+        mc_move = input_arguments['mc_move']
+        topology = input_arguments['topology']
+        n_iterations = input_arguments['n_iterations']
+        cycle_count = input_arguments['cycle_count']
+
+    except KeyError as e:
+        return #fill in a call back to the error queue
+
+    #now check if the optional arguments are present. If so, unpack them:
+    if 'atom_indices_to_save' in input_arguments.keys():
+        atom_indices_to_save = input_arguments['atom_indices_to_save']
+    else:
+        atom_indices_to_save = None
+
+    n_cycles = input_arguments['n_cycles'] if 'n_cycles' in input_arguments.keys() else 0
+
+    run_nonequilibrium = input_arguments['run_nonequilbrium'] if 'run_nonequilibrium' in input_arguments.keys() else False
+
+    #run the actual task
+    sampler_state_new, trajectory, reduced_potential_final_frame = feptasks.run_equilibrium(sampler_state,
+                                                                                            thermodynamic_state,
+                                                                                            mc_move, topology,
+                                                                                            n_iterations,
+                                                                                            atom_indices_to_save=atom_indices_to_save)
+    #increment cycle count:
+    cycle_count += 1
+    input_arguments['sampler_state'] = sampler_state_new
+    input_arguments['cycle_count'] = cycle_count
+    input_arguments['n_cycles'] = n_cycles - 1
+
+    #serialize to bytes
+    resume_task_input = BytesIO()
+    pickle.dump(input_arguments, resume_task_input)
+
+    #prepare the results:
+    #These results will go to the write queue
+    write_routing_key = ".".join([equilibrium_prefix_key, "trajectory"])
+    traj_results = dict()
+    traj_results['trajectory'] = trajectory
+    traj_results['cycle_count'] = cycle_count
+    traj_results_output = BytesIO()
+    pickle.dump(traj_results, traj_results_output)
+
+    #publish the results to the trajectory writing topic:
+    ch.basic_publish(exchange='equilibrium', routing_key=write_routing_key, body=traj_results_output.getvalue())
+
+    #We also want to publish the energy and some basic result status to the logging task:
+    energy_routing_key = ".".join([equilibrium_prefix_key, "energy_output"])
+    energy_results = dict()
+    energy_results['reduced_potential'] = reduced_potential_final_frame
+    energy_results['cycle_count'] = cycle_count
+    energy_results['sampler_state'] = sampler_state_new #include this so that the endpoint perturbation can be calculated
+    energy_output = BytesIO()
+    pickle.dump(energy_results, energy_output)
+
+    #publish this to the energy output topic:
+    ch.basic_publish(exchange="equilibrium", routing_key=energy_routing_key, body=energy_output.getvalue())
+
+    #if the number of equilibrium cycles requested is greater than zero, we should send another task to run another
+    #equilibrium cycle, and decrement the counter
+    if n_cycles > 0:
+        #Publish to resume topic
+        resume_routing_key = ".".join([equilibrium_prefix_key, "resume"])
+        ch.basic_publish(exchange="equilibrium", routing_key=resume_routing_key, body=resume_task_input.getvalue())
+
+    #finally, if the user requested nonequilibrium, send a message to the nonequilibrium task queue to run that.
+    #at this point, the input data structure needs to have included the appropriate mcmcmove for nonequilibrium
+    #switching--we cannot generate it here. If it's not there, the nonequilibrium switching task will return an error
+    if run_nonequilibrium:
+        nonequilibrium_routing_key = ".".join([nonequilibrium_prefix_key, "run"])
+
+        #the information needed to run this will also be in the resume task input.
+        ch.basic_publish(exchange="nonequilibrium", routing_key=nonequilibrium_routing_key, body=resume_task_input.getvalue())
+
+def nonequilibrium_callback(ch, method, properties, body):
+    pass
+
+def reduced_potential_callback(ch, method, properties, body):
+    pass
+
+if __name__=="__main__":
+    pass
